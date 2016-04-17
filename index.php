aee0c9c92b9c9002a8114720c31f8aba898ae58d
ViktorLomakin <jshepot@gmail.com>
2016-04-17 20:58:01 +0300

    Add footer for client.

--- index.php
+++ index.php
@@ -9,4 +9,12 @@
 	$user = 'ViktorLomakin';
 	// Current branch testing(not master)
 	echo 'Change branch by command: git: checkout name_of_branch';
+?>
+
+
+<div id="footer" class="main_footer">
+
+</div>
+
+
 
